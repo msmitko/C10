683cd05978502fd089cc8bffa64508418213aa99
msmitko <44364545+msmitko@users.noreply.github.com>
2018-12-03 10:49:50 +0200

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Loops(int[] arr, int index)
+    {
+        if (index >= arr.Length)
+        {
+            foreach (int element in arr) Console.Write("{0} ", element);
+            Console.WriteLine();
+        }
+        else
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                arr[index] = i;
+                Loops(arr, index + 1);
+            }
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter N: ");
+        int n = Int32.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        Loops(arr, 0);
+    }
+}
+
